bc0e42c2cc092cf4903f1783ed0de43f66b9dec0
Haythem Sellami <sellamihaythem96@gmail.com>
2019-11-13 16:16:29 +0100

    Liquidity Reserve Factory

---
+++ contracts/liquidity-reserve/factory/LiquidityReserveFactory.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.5.0;
+
+import { LiquidityReserve } from "../LiquidityReserve.sol";
+
+/**
+ * @title LiquidityReserveFactory
+ * @dev This Factory creates a Liquidity Reserve
+ */
+contract LiquidityReserveFactory {
+
+  /**
+   * @dev Creates and initialises a new LiquidityReserve
+   * @param _beneficiary earnings beneficiary (address(0) if the earnings goes to the current reserve address)
+   * @param _lockingPeriod funds locking period
+   * @param _lockingPrice release funds when hitting this price
+   * @param _lockingPricePosition locking price position
+   * @return Address of new Liquidity Reserve
+   */
+  function createLiquidityReserve(
+    address _beneficiary,
+    uint256 _lockingPeriod,
+    uint256 _lockingPrice,
+    uint8 _lockingPricePosition
+  ) public returns (address) {
+    return address(
+      new LiquidityReserve(
+        msg.sender,
+        _beneficiary,
+        _lockingPeriod,
+        _lockingPrice,
+        _lockingPricePosition
+      )
+    );
+  }
+}
