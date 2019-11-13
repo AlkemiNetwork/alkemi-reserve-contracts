5bc4082c46b2b6c7af1a3919c256b4c6b35d82b9
Haythem Sellami <sellamihaythem96@gmail.com>
2019-11-13 18:14:14 +0100

    update create reserve function

--- contracts/liquidity-reserve/factory/LiquidityReserveFactory.sol
+++ contracts/liquidity-reserve/factory/LiquidityReserveFactory.sol
@@ -10,6 +10,8 @@ contract LiquidityReserveFactory {
 
   /**
    * @dev Creates and initialises a new LiquidityReserve
+   * @param _liquidityReserveManager Lequidity Reserve Manager contract address
+   * @param _alkemiToken Alkemi token address
    * @param _beneficiary earnings beneficiary (address(0) if the earnings goes to the current reserve address)
    * @param _lockingPeriod funds locking period
    * @param _lockingPrice release funds when hitting this price
@@ -17,6 +19,8 @@ contract LiquidityReserveFactory {
    * @return Address of new Liquidity Reserve
    */
   function createLiquidityReserve(
+    address _liquidityReserveManager,
+    address _alkemiToken,
     address _beneficiary,
     uint256 _lockingPeriod,
     uint256 _lockingPrice,
@@ -25,6 +29,8 @@ contract LiquidityReserveFactory {
     return address(
       new LiquidityReserve(
         msg.sender,
+        _liquidityReserveManager,
+        _alkemiToken,
         _beneficiary,
         _lockingPeriod,
         _lockingPrice,
