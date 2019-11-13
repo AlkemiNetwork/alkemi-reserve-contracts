f6f4cb483c3c440c78fddb013c32eba67f5859e3
Haythem Sellami <sellamihaythem96@gmail.com>
2019-11-13 13:47:46 +0100

    init contracts interfaces

---
+++ contracts/interfaces/ILiquidityReserve.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+
+/**
+  * @title ILiquidityReserve
+  */
+contract ILiquidityReserve {
+}
---
+++ contracts/interfaces/ILiquidityReserveFactory.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+
+/**
+  * @title ILiquidityReserveFactory
+  */
+contract ILiquidityReserveFactory {
+}
