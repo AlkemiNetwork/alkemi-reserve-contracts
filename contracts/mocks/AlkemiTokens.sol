1135e01ac23cfc96fa7bf1b98109e84db0f0fa68
Haythem Sellami <sellamihaythem96@gmail.com>
2019-11-14 18:38:44 +0100

    token mock

---
+++ contracts/mocks/AlkemiTokens.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+
+/**
+ * @notice ERC20 Mintable token mock
+ */
+contract AlkemiToken is ERC20Mintable {
+}
