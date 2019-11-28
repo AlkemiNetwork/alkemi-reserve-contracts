5a65e2ab605eac333861222f260e2081e4623603
Haythem Sellami <sellamihaythem96@gmail.com>
2019-11-28 15:46:15 +0100

    modify oracle guard

--- contracts/oracles/OracleGuard.sol
+++ contracts/oracles/OracleGuard.sol
@@ -2,18 +2,24 @@ pragma solidity ^0.5.0;
 
 contract OracleGuard {
 
+  // Alkemi oracle contract address
   address internal _oracleContract;
 
+  // Alkemi token address
+  address internal _alkemiToken;
+
   // --- Auth ---
+  // 1: OracleGuard Owner permission
+  // 2: OracleContract permission
   mapping (address => uint) public auths;
-  
-  // Authorized oracles, set by an auth
-  mapping (address => uint256) public orcl;
 
   // Whitelisted contracts, set by an auth
   mapping (address => uint256) public contracts;
+  
+  // Mapping for off-chain nodes, set by an auth
+  mapping (address => uint256) public nodes;
 
-  // Mapping for oracles
+  // Authorized off-chain nodes, set by an auth
   mapping (uint256 => address) public slot;
 
   constructor() public {
@@ -21,13 +27,18 @@ contract OracleGuard {
   }
 
   modifier auth {
-    require(auths[msg.sender] == 1, "OracleGuard: not authorized");
+    require(
+      (auths[msg.sender] == 1) || (auths[msg.sender] == 2),
+      "OracleGuard: not authorized");
     _;
   }
 
-  modifier isAuthContract {
-    require(contracts[msg.sender] == 1, "OracleGuard: contract not whitelisted");
-    _;
+  function isContractAuth(address _contract) external view returns (bool) {
+    return (contracts[_contract] == 1);
+  }
+
+  function isNodeAuth(address _node) external view returns (bool) {
+    return ((nodes[_node] == 1) && (slot[uint8(uint256(_node) >> 152)] == _node));
   }
 
   function rely(address usr) external auth {
@@ -38,19 +49,32 @@ contract OracleGuard {
     auths[usr] = 0;
   }
 
-  function authOracles(address[] calldata a) external auth {
+  function authNode(address a) external auth {
+    require(a != address(0), "OracleGuard: invalid oracle address");
+    uint8 s = uint8(uint256(a) >> 152);
+    require(slot[s] == address(0), "OracleGuard: oracle already authorized");
+    nodes[a] = 1;
+    slot[s] = a;
+  }
+
+  function dropNode(address a) external auth {
+    //nodes[a[i]] = 0;
+    slot[uint8(uint256(a) >> 152)] = address(0);
+  }
+
+  function authNode(address[] calldata a) external auth {
     for (uint i = 0; i < a.length; i++) {
       require(a[i] != address(0), "OracleGuard: invalid oracle address");
       uint8 s = uint8(uint256(a[i]) >> 152);
-      require(slot[s] == address(0), "OracleGuard: oracle already exists");
-      orcl[a[i]] = 1;
+      require(slot[s] == address(0), "OracleGuard: oracle already authorized");
+      nodes[a[i]] = 1;
       slot[s] = a[i];
     }
   }
 
-  function dropOracles(address[] calldata a) external auth {
+  function dropNode(address[] calldata a) external auth {
     for (uint i = 0; i < a.length; i++) {
-      orcl[a[i]] = 0;
+      //nodes[a[i]] = 0;
       slot[uint8(uint256(a[i]) >> 152)] = address(0);
     }
   }
@@ -77,13 +101,27 @@ contract OracleGuard {
     }
   }
 
-  function setOracleContract(address oracleContract) public auth {
+  function setOracleContract(address oracleContract) external auth {
     require(oracleContract != address(0), "OracleGuard: invalid address");
 
+    // Remove oracle permissions
+    auths[_oracleContract] = 0;
+
     _oracleContract = oracleContract;
+    auths[oracleContract] = 2;
+  }
+
+  function setTokenContract(address alkemiToken) external auth {
+    require(alkemiToken != address(0), "OracleGuard: invalid address");
+
+    _alkemiToken = alkemiToken;
   }
 
   function oracle() public view returns (address) {
     return _oracleContract;
+  }
+
+  function token() public view returns (address) {
+    return _alkemiToken;
   }
 }
