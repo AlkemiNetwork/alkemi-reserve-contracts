2b84e155091aa1f33d0f06ad603f45284aba801b
Haythem Sellami <sellamihaythem96@gmail.com>
2019-12-03 16:03:20 +0100

    update names

--- contracts/mocks/TokenMock.sol
+++ contracts/mocks/TokenMock.sol
@@ -5,5 +5,5 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 /**
  * @notice ERC20 Mintable token mock
  */
-contract Token is ERC20Mintable {
+contract TokenMock is ERC20Mintable {
 }
