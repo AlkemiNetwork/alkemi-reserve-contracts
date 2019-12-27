13fbbce808994baa163e095e377dd2072a7ef5ce
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-27 11:41:47 +0000

    Deploy project-alkemi/alkemi-protocol to github.com/project-alkemi/alkemi-protocol.git:gh-pages

--- .solcover.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    norpc: true,
-    copyPackages: [],
-    buildDirPath: '/build/contracts',
-    testCommand: 'ganache-cli --port 8555 --gasPrice 0x01 --gasLimit 0xfffffffffff > /dev/null & truffle test --network coverage',
-    skipFiles: [
-        'Migrations.sol'
-    ]
-}
--- contracts/interfaces/IOracle.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-
-/**
- * @title IOracle
- */
-contract IOracle {
-  function restartContainersTrading(uint256 settlementId, uint256 settlementTime) external;
-}
