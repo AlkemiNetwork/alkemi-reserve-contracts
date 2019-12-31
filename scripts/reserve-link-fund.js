3d0185a8878949fba3011b1c393fc7f3e90c5726
Haythem Sellami <haythem@alkemi.ai>
2019-12-31 12:23:30 +0100

    CD-197 script to fund liquidity reserve with LINK token

---
+++ scripts/reserve-link-fund.js
@@ -0,0 +1,20 @@
+const { helpers, generated } = require("chainlink");
+const LiquidityReserve = artifacts.require("LiquidityReserve");
+
+/*
+  This script is meant to assist with funding the liquidity
+  reserve contract with LINK. It will send 1 LINK to the requesting
+  contract for ease-of-use. Any extra LINK present on the contract
+  can be retrieved by calling the withdrawLink() function.
+*/
+
+const payment = process.env.TRUFFLE_CL_BOX_PAYMENT || '1000000000000000000';
+const reserveAddress = process.env.LIQUIDITY_RESERVE_ADDRESS;
+
+module.exports = async callback => {
+  const linkToken = await helpers.create(generated.LinkTokenFactory, alkemiTeam).deploy();
+  const liquidityReserve = await LiquidityReserve.at(reserveAddress);
+  console.log('Funding contract:', liquidityReserve.address);
+  const tx = await linkToken.transfer(liquidityReserve.address, payment);
+  callback(tx.tx);
+}
